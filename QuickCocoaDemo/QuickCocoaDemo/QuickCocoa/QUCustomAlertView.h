babf5dc3861583b55e54d4345e9f36fae0233517
BriceZH <mac@MACdeiMac.local>
2018-07-19 15:11:51 +0800

    提交自定义弹窗

---
+++ QuickCocoaDemo/QuickCocoaDemo/QuickCocoa/QUCustomAlertView.h
@@ -0,0 +1,13 @@
+//
+//  QUCustomAlertView.h
+//  QuickCocoaDemo
+//
+//  Created by BriceZH on 2018/7/19.
+//  Copyright © 2018年 BriceZhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QUCustomAlertView : UIView
+
+@end
---
+++ QuickCocoaDemo/QuickCocoaDemo/QuickCocoa/QUCustomAlertView.m
@@ -0,0 +1,21 @@
+//
+//  QUCustomAlertView.m
+//  QuickCocoaDemo
+//
+//  Created by BriceZH on 2018/7/19.
+//  Copyright © 2018年 BriceZhao. All rights reserved.
+//
+
+#import "QUCustomAlertView.h"
+
+@implementation QUCustomAlertView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
